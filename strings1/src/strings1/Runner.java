48c87e5ca9fa9cdb09eb6f80b878d30ca964c955
Tezozomoctzin <pokerfaceblr@gmail.com>
2015-04-03 16:13:38 -0300

    03.04.2015.1

--- strings1/src/strings1/Runner.java
+++ strings1/src/strings1/Runner.java
@@ -9,98 +9,100 @@ import javax.xml.transform.ErrorListener;
 import by.gsu.epamlab.Constants;
 
 public class Runner {
-		int errorLines;
-		double summ = 0;
-		StringBuilder output = new StringBuilder();
-		StringBuilder errorsLine = new StringBuilder();
-		boolean firstElementNegative = false;
+	private int errorLines;
+	private double summ = 0;
+	private StringBuilder output = new StringBuilder();
+	private StringBuilder errorsLine = new StringBuilder();
+	private boolean isFirstElementNegative = false;
+
 	public static void main(String[] args) {
 		Runner run = new Runner();
 		File file = new File("src/in.csv");
 		Scanner input = null;
 		String line;
 		String[] splitLine;
-		int lineElement;	
+		int lineElement;
 		try {
 			input = new Scanner(file);
-			while(input.hasNext()){
+			while (input.hasNext()) {
 				line = input.nextLine();
-				if(line.length() != 0){
+				if (line.length() != 0) {
 					splitLine = line.split(";");
-						lineElement = (int) Math.floor(run.parsingTry(((splitLine[0]))));
-						run.lineElementHandling(lineElement, splitLine);			
-				}else{
+					lineElement = (int) Math.floor(run
+							.parsingTry(((splitLine[0]))));
+					run.lineElementHandling(lineElement, splitLine);
+				} else {
 					run.errorLines++;
 				}
 			}
-			run.appendLines(run.firstElementNegative);
+			run.appendLines(run.isFirstElementNegative);
 			System.out.println(run.output.toString());
 			System.out.println(run.errorsLine.toString());
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found\n");
 			e.printStackTrace();
-		}finally{
-			if(input != null){
+		} finally {
+			if (input != null) {
 				input.close();
 			}
-		}		
+		}
 	}
 
-	public double parsingTry(String str){
-		if(isNumber(str)){
+	public double parsingTry(String str) {
+		if (isNumber(str)) {
 			return Double.parseDouble(str);
-		}else{
+		} else {
 			return -1;
 		}
 	}
-	
-	public boolean isNumber(String verify){
-		try{
+
+	public boolean isNumber(String verify) {
+		try {
 			Double.parseDouble(verify);
 			return true;
-		}catch (NumberFormatException e){
+		} catch (NumberFormatException e) {
 			return false;
 		}
 	}
-	
-	 public void lineElementHandling(int lineElement, String[] line){
-		 if(lineElement >= 0){
-				try{
-					double determinedElement = Double.parseDouble(line[lineElement]);	
-					summ += determinedElement;
-					if( output.length() > 0)
-					{
-						if(determinedElement >=0){
-							output.append(Constants.PLUS);
-						}else{
-							output.append(Constants.MINUS);
-						}
-					}else{
-						if(!(determinedElement >= 0)){
-							firstElementNegative = true;
-						}
+
+	public void lineElementHandling(int lineElement, String[] line) {
+		if (lineElement >= 0) {
+			try {
+				double determinedElement = Double
+						.parseDouble(line[lineElement]);
+				summ += determinedElement;
+				if (output.length() > 0) {
+					if (determinedElement >= 0) {
+						output.append(Constants.PLUS);
+					} else {
+						output.append(Constants.MINUS);
 					}
-					output.append(Math.abs(determinedElement));
-				}catch (RuntimeException e){
-					errorLines ++;
+				} else {
+					if (!(determinedElement >= 0)) {
+						isFirstElementNegative = true;
+					}
 				}
-			}else{
+				output.append(Math.abs(determinedElement));
+			} catch (RuntimeException e) {
 				errorLines++;
-			}						
-	 }
-	
-	 public void appendLines(boolean flag){
-		 if(firstElementNegative){
-				output.insert(0, "-");
 			}
-			output.insert(0, "result(");
-			output.append(")");
-			output.append(Constants.EQUALS);
-			output.append(String.format("%.2f", summ));
-			errorsLine.ensureCapacity(output.capacity());
-			errorsLine.append(Constants.EQUALS);
-			errorsLine.insert(0, "error-lines");
-			errorsLine.append(String.format("%d",errorLines));
-	 }
-	 
+		} else {
+			errorLines++;
+		}
+	}
+
+	public void appendLines(boolean flag) {
+		if (flag) {
+			output.insert(0, "-");
+		}
+		output.insert(0, "result(");
+		output.append(")");
+		output.append(Constants.EQUALS);
+		output.append(String.format("%.2f", summ));
+		errorsLine.ensureCapacity(output.capacity());
+		errorsLine.append(Constants.EQUALS);
+		errorsLine.insert(0, "error-lines");
+		errorsLine.append(String.format("%d", errorLines));
+	}
+
 }
