e9a9355d4a7933c673c69c9696d1e335ae842dc0
Tezozomoctzin <pokerfaceblr@gmail.com>
2015-04-03 05:23:15 -0300

    03.04.2015.1

--- strings1/src/strings1/Runner.java
+++ strings1/src/strings1/Runner.java
@@ -4,61 +4,93 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import javax.xml.transform.ErrorListener;
+
 import by.gsu.epamlab.Constants;
 
 public class Runner {
-
+		int errorLines;
+		double summ = 0;
+		StringBuilder output = new StringBuilder();
+		StringBuilder errorsLine = new StringBuilder();
+		boolean firstElementNegative = false;
 	public static void main(String[] args) {
+		Runner run = new Runner();
 		File file = new File("src/in.csv");
 		Scanner input = null;
 		String line;
 		String[] splitLine;
-		int lineElement;
-		int errorLines = 0;
-		double summ = 0;
-		double determinedElement;
-		boolean firstElementNegative = false;
-		StringBuilder output = new StringBuilder();
-		StringBuilder errorsLine = new StringBuilder();
+		int lineElement;	
 		try {
 			input = new Scanner(file);
 			while(input.hasNext()){
 				line = input.nextLine();
 				if(line.length() != 0){
 					splitLine = line.split(";");
-					try{
-						lineElement = (int) Math.floor(Double.parseDouble(splitLine[0]));
-						if(lineElement >= 0){
-							try{
-								determinedElement = Double.parseDouble(splitLine[lineElement]);	
-								summ += determinedElement;
-								if( output.length() > 0)
-								{
-									if(determinedElement >=0){
-										output.append(Constants.PLUS);
-									}else{
-										output.append(Constants.MINUS);
-									}
-								}else{
-									if(!(determinedElement >= 0)){
-										firstElementNegative = true;
-									}
-								}
-								output.append(Math.abs(determinedElement));
-							}catch (RuntimeException e){
-								errorLines++;
-							}
-						}else{
-							errorLines++;
-						}
-					}catch (IllegalArgumentException e){
-						errorLines++;
-					}								
+						lineElement = (int) Math.floor(run.parsingTry(((splitLine[0]))));
+						run.lineElementHandling(lineElement, splitLine);			
 				}else{
-					errorLines++;
+					run.errorLines++;
 				}
 			}
-			if(firstElementNegative){
+			run.appendLines(run.firstElementNegative);
+			System.out.println(run.output.toString());
+			System.out.println(run.errorsLine.toString());
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found\n");
+			e.printStackTrace();
+		}finally{
+			if(input != null){
+				input.close();
+			}
+		}		
+	}
+
+	public double parsingTry(String str){
+		if(isNumber(str)){
+			return Double.parseDouble(str);
+		}else{
+			return -1;
+		}
+	}
+	
+	public boolean isNumber(String verify){
+		try{
+			Double.parseDouble(verify);
+			return true;
+		}catch (NumberFormatException e){
+			return false;
+		}
+	}
+	
+	 public void lineElementHandling(int lineElement, String[] line){
+		 if(lineElement >= 0){
+				try{
+					double determinedElement = Double.parseDouble(line[lineElement]);	
+					summ += determinedElement;
+					if( output.length() > 0)
+					{
+						if(determinedElement >=0){
+							output.append(Constants.PLUS);
+						}else{
+							output.append(Constants.MINUS);
+						}
+					}else{
+						if(!(determinedElement >= 0)){
+							firstElementNegative = true;
+						}
+					}
+					output.append(Math.abs(determinedElement));
+				}catch (RuntimeException e){
+					errorLines ++;
+				}
+			}else{
+				errorLines++;
+			}						
+	 }
+	
+	 public void appendLines(boolean flag){
+		 if(firstElementNegative){
 				output.insert(0, "-");
 			}
 			output.insert(0, "result(");
@@ -69,16 +101,6 @@ public class Runner {
 			errorsLine.append(Constants.EQUALS);
 			errorsLine.insert(0, "error-lines");
 			errorsLine.append(String.format("%d",errorLines));
-			System.out.println(output.toString());
-			System.out.println(errorsLine.toString());
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found\n");
-			e.printStackTrace();
-		}finally{
-			if(input != null){
-				input.close();
-			}
-		}
-	}
-
+	 }
+	 
 }
