282b66796e54c493c325d3983334cc7ab2e68780
Tezozomoctzin <pokerfaceblr@gmail.com>
2015-04-09 22:51:38 +0300

    09.04.2015.2251

--- collections1/src/by/gsu/epamlab/CsvLineException.java
+++ collections1/src/by/gsu/epamlab/CsvLineException.java
@@ -1,5 +1,14 @@
 package by.gsu.epamlab;
 
 public class CsvLineException extends Exception {
+	private String exceptionString;
+
+	public CsvLineException() {
+		super();
+	}
+
+	public CsvLineException(Exception exception, String errorString) {
+
+	}
 
 }
